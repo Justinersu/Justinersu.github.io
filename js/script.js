936378d38dca48aed5548c076a68dc5c41018810
Justinersu <justineroussoeau@gmail.com>
2024-10-11 15:05:45 -0400

    commencement du fullscreen

--- js/script.js
+++ js/script.js
@@ -1,64 +1,11 @@
-/* ------ Switcher entre les images ------
-let imgPortrait = document.getElementById("imgPortrait");
-let btnDroit = document.getElementById("btnDroit");
-let btnGauche = document.getElementById("btnGauche");
-
-function nextImgDroit() {
-    if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/portrait_1_mode.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/portrait_2_musique.jpg");
-    }
-    else if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/portrait_2_musique.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/portrait_3_nature.jpg")
-    }
-    else if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/portrait_3_nature.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/portrait_4_lecture.jpg")
-    } else if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/portrait_4_lecture.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/portrait_5_jeux_video.jpg")
-    } else if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/portrait_5_jeux_video.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/portrait_6_beaute.jpg")
-    } else if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/portrait_6_beaute.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/signature.jpg")
-    } else if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/signature.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/moodboard_portrait.jpg")
-    } else if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/moodboard_portrait.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/moodboard_signature.jpg")
-    } else if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/moodboard_signature.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/portrait_1_mode.jpg")
-    }
-}
-
-function nextImgGauche() {
-    if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/portrait_1_mode.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/moodboard_signature.jpg");
-    }
-    else if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/moodboard_signature.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/moodboard_portrait.jpg")
-    }
-    else if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/moodboard_portrait.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/signature.jpg")
-    } else if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/signature.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/portrait_6_beaute.jpg")
-    } else if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/portrait_6_beaute.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/portrait_5_jeux_video.jpg")
-    } else if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/portrait_5_jeux_video.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/portrait_4_lecture.jpg")
-    } else if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/portrait_4_lecture.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/portrait_3_nature.jpg")
-    } else if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/portrait_3_nature.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/portrait_2_musique.jpg")
-    } else if (imgPortrait.getAttribute('src') === "./assets/projet_portrait/portrait_2_musique.jpg") {
-        imgPortrait.setAttribute('src', "./assets/projet_portrait/portrait_1_mode.jpg")
-    }
-}
-
-btnDroit.addEventListener("click", nextImgDroit);
-
-btnGauche.addEventListener("click", nextImgGauche); */
-
 // ------ Switcher entre les images ------
 let imgPortrait = document.getElementById("imgPortrait");
 let btnDroit = document.getElementById("btnDroit");
 let btnGauche = document.getElementById("btnGauche");
+
+let imgScene = document.getElementById("imgScene");
+let btnSceneDroit = document.getElementById("btnSceneDroit");
+let btnSceneGauche = document.getElementById("btnSceneGauche");
 
 const portraits = [
     "./assets/projet_portrait/portrait_1_mode.jpg",
@@ -72,24 +19,71 @@ const portraits = [
     "./assets/projet_portrait/moodboard_signature.jpg"
 ];
 
-let currentIndex = 0;
+const scenes = [
+    "./assets/projet_3d/rendu_global.jpg",
+    "./assets/projet_3d/rendu_coffre.jpg",
+    "./assets/projet_3d/rendu_lit.jpg",
+    "./assets/projet_3d/rendu_potion.jpg"
+];
+
+let currentIndexPortrait = 0;
+let currentIndexScene = 0;
 
 function updateImage() {
-    imgPortrait.setAttribute('src', portraits[currentIndex]);
+    imgPortrait.setAttribute('src', portraits[currentIndexPortrait]);
+}
+
+function updateScene() {
+    imgScene.setAttribute('src', scenes[currentIndexScene]);
 }
 
 function nextImgDroit() {
-    currentIndex = (currentIndex + 1) % portraits.length; // Va à la prochaine image
+    currentIndexPortrait = (currentIndexPortrait + 1) % portraits.length; // Va à la prochaine image
     updateImage();
 }
 
 function nextImgGauche() {
-    currentIndex = (currentIndex - 1 + portraits.length) % portraits.length; // Va à l'image précédente
+    currentIndexPortrait = (currentIndexPortrait - 1 + portraits.length) % portraits.length; // Va à l'image précédente
     updateImage();
+}
+
+function nextSceneDroit() {
+    currentIndexScene = (currentIndexScene + 1) % scenes.length; // Va à la prochaine scène
+    updateScene();
+}
+
+function nextSceneGauche() {
+    currentIndexScene = (currentIndexScene - 1 + scenes.length) % scenes.length; // Va à la scène précédente
+    updateScene();
 }
 
 btnDroit.addEventListener("click", nextImgDroit);
 btnGauche.addEventListener("click", nextImgGauche);
+btnSceneDroit.addEventListener("click", nextSceneDroit);
+btnSceneGauche.addEventListener("click", nextSceneGauche);
 
-// Initialize the first image
+// Initialiser la première image
 updateImage();
+updateScene();
+
+// ----- Fullscreen -----
+let body = document.querySelector("body");
+let btnFullscreen = document.getElementById("btnFullscreen");
+const fullscreen = document.querySelector(".fullscreen");
+let btnFermer = document.getElementById("btnFermer");
+let imgFullscreen = document.getElementById("imgFullscreen");
+
+function appearFullscreen() {
+    imgFullscreen.setAttribute('src', scenes[currentIndexScene]);
+    body.classList.add('stop-scrolling');
+    fullscreen.style.display = "block";
+}
+
+function noFullscreen() {
+    body.classList.remove('stop-scrolling');
+    fullscreen.style.display = "none";
+}
+
+btnFullscreen.addEventListener("click", appearFullscreen)
+
+btnFermer.addEventListener("click", noFullscreen)
